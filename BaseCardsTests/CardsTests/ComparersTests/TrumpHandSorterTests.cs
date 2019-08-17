1c5cafdc8c6c2656d32cc40cab41043c0a8bf8a0
Joe Meyer <joemeyer@exchangecore.com>
2019-08-16 23:13:35 -0400

    updating tests
    Adding license
    
    Adding travis yml
    
    changing travis yml
    
    adjusting travis yml again
    
    Travis builds

--- BaseCardsTests/CardsTests/ComparersTests/TrumpHandSorterTests.cs
+++ BaseCardsTests/CardsTests/ComparersTests/TrumpHandSorterTests.cs
@@ -34,85 +34,116 @@ namespace BaseCardsTests.CardsTests.ComparersTests
         }
         
         [Theory]
-        [InlineData(SuitEnum.Diamonds, RankEnum.Ace, SuitEnum.Diamonds, RankEnum.Ace, 0)]
-        [InlineData(SuitEnum.Diamonds, RankEnum.Ace, SuitEnum.Diamonds, RankEnum.King, 1)]
-        [InlineData(SuitEnum.Diamonds, RankEnum.Ace, SuitEnum.Diamonds, RankEnum.Queen, 1)]
-        [InlineData(SuitEnum.Diamonds, RankEnum.Ace, SuitEnum.Diamonds, RankEnum.Jack, 1)]
-        [InlineData(SuitEnum.Diamonds, RankEnum.Ace, SuitEnum.Diamonds, RankEnum.Ten, 1)]
-        [InlineData(SuitEnum.Diamonds, RankEnum.Ace, SuitEnum.Diamonds, RankEnum.Nine, 1)]
-        [InlineData(SuitEnum.Diamonds, RankEnum.King, SuitEnum.Diamonds, RankEnum.Queen, 1)]
-        [InlineData(SuitEnum.Diamonds, RankEnum.King, SuitEnum.Diamonds, RankEnum.Jack, 1)]
-        [InlineData(SuitEnum.Diamonds, RankEnum.King, SuitEnum.Diamonds, RankEnum.Ten, 1)]
-        [InlineData(SuitEnum.Diamonds, RankEnum.King, SuitEnum.Diamonds, RankEnum.Nine, 1)]
-        [InlineData(SuitEnum.Diamonds, RankEnum.Queen, SuitEnum.Diamonds, RankEnum.Jack, 1)]
-        [InlineData(SuitEnum.Diamonds, RankEnum.Queen, SuitEnum.Diamonds, RankEnum.Ten, 1)]
-        [InlineData(SuitEnum.Diamonds, RankEnum.Queen, SuitEnum.Diamonds, RankEnum.Nine, 1)]
-        [InlineData(SuitEnum.Diamonds, RankEnum.Jack, SuitEnum.Diamonds, RankEnum.Ten, 1)]
-        [InlineData(SuitEnum.Diamonds, RankEnum.Jack, SuitEnum.Diamonds, RankEnum.Nine, 1)]
-        [InlineData(SuitEnum.Diamonds, RankEnum.Ten, SuitEnum.Diamonds, RankEnum.Nine, 1)]
-        [InlineData(SuitEnum.Diamonds, RankEnum.Ace, SuitEnum.Clubs, RankEnum.Ace, -1)]
-        [InlineData(SuitEnum.Diamonds, RankEnum.Ace, SuitEnum.Hearts, RankEnum.Ace, -1)]
-        [InlineData(SuitEnum.Diamonds, RankEnum.Ace, SuitEnum.Spades, RankEnum.Ace, -1)]
-        [InlineData(SuitEnum.Clubs, RankEnum.Ace, SuitEnum.Hearts, RankEnum.Ace, -1)]
-        [InlineData(SuitEnum.Clubs, RankEnum.Ace, SuitEnum.Spades, RankEnum.Ace, -1)]
-        [InlineData(SuitEnum.Hearts, RankEnum.Ace, SuitEnum.Spades, RankEnum.Ace, -1)]
+        [InlineData(SuitEnum.Diamonds, RankEnum.Jack, SuitEnum.Diamonds, RankEnum.Jack, SuitEnum.Diamonds, 0)]
+        [InlineData(SuitEnum.Diamonds, RankEnum.Jack, SuitEnum.Hearts, RankEnum.Jack, SuitEnum.Diamonds, 1)]
+        [InlineData(SuitEnum.Diamonds, RankEnum.Jack, SuitEnum.Diamonds, RankEnum.Ace, SuitEnum.Diamonds, 1)]
+        [InlineData(SuitEnum.Diamonds, RankEnum.Jack, SuitEnum.Diamonds, RankEnum.King, SuitEnum.Diamonds, 1)]
+        [InlineData(SuitEnum.Diamonds, RankEnum.Jack, SuitEnum.Diamonds, RankEnum.Queen, SuitEnum.Diamonds, 1)]
+        [InlineData(SuitEnum.Diamonds, RankEnum.Jack, SuitEnum.Diamonds, RankEnum.Ten, SuitEnum.Diamonds, 1)]
+        [InlineData(SuitEnum.Diamonds, RankEnum.Jack, SuitEnum.Diamonds, RankEnum.Nine, SuitEnum.Diamonds, 1)]
+        [InlineData(SuitEnum.Hearts, RankEnum.Jack, SuitEnum.Diamonds, RankEnum.Ace, SuitEnum.Diamonds, 1)]
+        [InlineData(SuitEnum.Hearts, RankEnum.Jack, SuitEnum.Diamonds, RankEnum.King, SuitEnum.Diamonds, 1)]
+        [InlineData(SuitEnum.Hearts, RankEnum.Jack, SuitEnum.Diamonds, RankEnum.Queen, SuitEnum.Diamonds, 1)]
+        [InlineData(SuitEnum.Hearts, RankEnum.Jack, SuitEnum.Diamonds, RankEnum.Ten, SuitEnum.Diamonds, 1)]
+        [InlineData(SuitEnum.Hearts, RankEnum.Jack, SuitEnum.Diamonds, RankEnum.Nine, SuitEnum.Diamonds, 1)]
+        [InlineData(SuitEnum.Diamonds, RankEnum.Ace, SuitEnum.Diamonds, RankEnum.King, SuitEnum.Diamonds, 1)]
+        [InlineData(SuitEnum.Diamonds, RankEnum.Ace, SuitEnum.Diamonds, RankEnum.Queen, SuitEnum.Diamonds, 1)]
+        [InlineData(SuitEnum.Diamonds, RankEnum.Ace, SuitEnum.Diamonds, RankEnum.Ten, SuitEnum.Diamonds, 1)]
+        [InlineData(SuitEnum.Diamonds, RankEnum.Ace, SuitEnum.Diamonds, RankEnum.Nine, SuitEnum.Diamonds, 1)]
+        [InlineData(SuitEnum.Diamonds, RankEnum.King, SuitEnum.Diamonds, RankEnum.Queen, SuitEnum.Diamonds, 1)]
+        [InlineData(SuitEnum.Diamonds, RankEnum.King, SuitEnum.Diamonds, RankEnum.Ten, SuitEnum.Diamonds, 1)]
+        [InlineData(SuitEnum.Diamonds, RankEnum.King, SuitEnum.Diamonds, RankEnum.Nine, SuitEnum.Diamonds, 1)]
+        [InlineData(SuitEnum.Diamonds, RankEnum.Queen, SuitEnum.Diamonds, RankEnum.Ten, SuitEnum.Diamonds, 1)]
+        [InlineData(SuitEnum.Diamonds, RankEnum.Queen, SuitEnum.Diamonds, RankEnum.Nine, SuitEnum.Diamonds, 1)]
+        [InlineData(SuitEnum.Diamonds, RankEnum.Ten, SuitEnum.Diamonds, RankEnum.Nine, SuitEnum.Diamonds, 1)]
+        [InlineData(SuitEnum.Diamonds, RankEnum.Ace, SuitEnum.Clubs, RankEnum.Ace, SuitEnum.Diamonds, -1)]
+        [InlineData(SuitEnum.Diamonds, RankEnum.Ace, SuitEnum.Hearts, RankEnum.Ace, SuitEnum.Diamonds, -1)]
+        [InlineData(SuitEnum.Diamonds, RankEnum.Ace, SuitEnum.Spades, RankEnum.Ace, SuitEnum.Diamonds, -1)]
+        [InlineData(SuitEnum.Clubs, RankEnum.Ace, SuitEnum.Clubs, RankEnum.Ace, SuitEnum.Diamonds, 0)]
+        [InlineData(SuitEnum.Clubs, RankEnum.Ace, SuitEnum.Clubs, RankEnum.King, SuitEnum.Diamonds, 1)]
+        [InlineData(SuitEnum.Clubs, RankEnum.Ace, SuitEnum.Clubs, RankEnum.Queen, SuitEnum.Diamonds, 1)]
+        [InlineData(SuitEnum.Clubs, RankEnum.Ace, SuitEnum.Clubs, RankEnum.Jack, SuitEnum.Diamonds, 1)]
+        [InlineData(SuitEnum.Clubs, RankEnum.Ace, SuitEnum.Clubs, RankEnum.Ten, SuitEnum.Diamonds, 1)]
+        [InlineData(SuitEnum.Clubs, RankEnum.Ace, SuitEnum.Clubs, RankEnum.Nine, SuitEnum.Diamonds, 1)]
+        [InlineData(SuitEnum.Clubs, RankEnum.Ace, SuitEnum.Hearts, RankEnum.Ace, SuitEnum.Diamonds, -1)]
+        [InlineData(SuitEnum.Clubs, RankEnum.Ace, SuitEnum.Spades, RankEnum.Ace, SuitEnum.Diamonds, -1)]
+        [InlineData(SuitEnum.Hearts, RankEnum.Ace, SuitEnum.Spades, RankEnum.Ace, SuitEnum.Diamonds, -1)]
         public void Compare_AscendingSuitAscendingRank(SuitEnum card1Suit, RankEnum card1Rank, SuitEnum card2Suit,
-            RankEnum card2Rank, int expectedCompareValue)
+            RankEnum card2Rank, SuitEnum trumpSuit, int expectedCompareValue)
         {
             var card1 = new Card(
                 new Rank(card1Rank, null), 
-                new Suit(' ', card1Suit, SuitEnum.Unknown)
+                FromFactory(card1Suit)
                 );
             var card2 = new Card(
                 new Rank(card2Rank, null), 
-                new Suit(' ', card2Suit, SuitEnum.Unknown)
+                FromFactory(card2Suit)
                 );
 
-            var comparer = new NoTrumpHandSorter(true, true);
+            var comparer = new TrumpHandSorter(FromFactory(trumpSuit), true, true);
             var comparison = comparer.Compare(card1, card2);
             
             Assert.Equal(expectedCompareValue, comparison);
         }
         
+        
         [Theory]
-        [InlineData(SuitEnum.Diamonds, RankEnum.Ace, SuitEnum.Diamonds, RankEnum.Ace, 0)]
-        [InlineData(SuitEnum.Diamonds, RankEnum.Ace, SuitEnum.Diamonds, RankEnum.King, -1)]
-        [InlineData(SuitEnum.Diamonds, RankEnum.Ace, SuitEnum.Diamonds, RankEnum.Queen, -1)]
-        [InlineData(SuitEnum.Diamonds, RankEnum.Ace, SuitEnum.Diamonds, RankEnum.Jack, -1)]
-        [InlineData(SuitEnum.Diamonds, RankEnum.Ace, SuitEnum.Diamonds, RankEnum.Ten, -1)]
-        [InlineData(SuitEnum.Diamonds, RankEnum.Ace, SuitEnum.Diamonds, RankEnum.Nine, -1)]
-        [InlineData(SuitEnum.Diamonds, RankEnum.King, SuitEnum.Diamonds, RankEnum.Queen, -1)]
-        [InlineData(SuitEnum.Diamonds, RankEnum.King, SuitEnum.Diamonds, RankEnum.Jack, -1)]
-        [InlineData(SuitEnum.Diamonds, RankEnum.King, SuitEnum.Diamonds, RankEnum.Ten, -1)]
-        [InlineData(SuitEnum.Diamonds, RankEnum.King, SuitEnum.Diamonds, RankEnum.Nine, -1)]
-        [InlineData(SuitEnum.Diamonds, RankEnum.Queen, SuitEnum.Diamonds, RankEnum.Jack, -1)]
-        [InlineData(SuitEnum.Diamonds, RankEnum.Queen, SuitEnum.Diamonds, RankEnum.Ten, -1)]
-        [InlineData(SuitEnum.Diamonds, RankEnum.Queen, SuitEnum.Diamonds, RankEnum.Nine, -1)]
-        [InlineData(SuitEnum.Diamonds, RankEnum.Jack, SuitEnum.Diamonds, RankEnum.Ten, -1)]
-        [InlineData(SuitEnum.Diamonds, RankEnum.Jack, SuitEnum.Diamonds, RankEnum.Nine, -1)]
-        [InlineData(SuitEnum.Diamonds, RankEnum.Ten, SuitEnum.Diamonds, RankEnum.Nine, -1)]
-        [InlineData(SuitEnum.Diamonds, RankEnum.Ace, SuitEnum.Clubs, RankEnum.Ace, 1)]
-        [InlineData(SuitEnum.Diamonds, RankEnum.Ace, SuitEnum.Hearts, RankEnum.Ace, 1)]
-        [InlineData(SuitEnum.Diamonds, RankEnum.Ace, SuitEnum.Spades, RankEnum.Ace, 1)]
-        [InlineData(SuitEnum.Clubs, RankEnum.Ace, SuitEnum.Hearts, RankEnum.Ace, 1)]
-        [InlineData(SuitEnum.Clubs, RankEnum.Ace, SuitEnum.Spades, RankEnum.Ace, 1)]
-        [InlineData(SuitEnum.Hearts, RankEnum.Ace, SuitEnum.Spades, RankEnum.Ace, 1)]
+        [InlineData(SuitEnum.Diamonds, RankEnum.Jack, SuitEnum.Diamonds, RankEnum.Jack, SuitEnum.Diamonds, 0)]
+        [InlineData(SuitEnum.Diamonds, RankEnum.Jack, SuitEnum.Hearts, RankEnum.Jack, SuitEnum.Diamonds, -1)]
+        [InlineData(SuitEnum.Diamonds, RankEnum.Jack, SuitEnum.Diamonds, RankEnum.Ace, SuitEnum.Diamonds, -1)]
+        [InlineData(SuitEnum.Diamonds, RankEnum.Jack, SuitEnum.Diamonds, RankEnum.King, SuitEnum.Diamonds, -1)]
+        [InlineData(SuitEnum.Diamonds, RankEnum.Jack, SuitEnum.Diamonds, RankEnum.Queen, SuitEnum.Diamonds, -1)]
+        [InlineData(SuitEnum.Diamonds, RankEnum.Jack, SuitEnum.Diamonds, RankEnum.Ten, SuitEnum.Diamonds, -1)]
+        [InlineData(SuitEnum.Diamonds, RankEnum.Jack, SuitEnum.Diamonds, RankEnum.Nine, SuitEnum.Diamonds, -1)]
+        [InlineData(SuitEnum.Hearts, RankEnum.Jack, SuitEnum.Diamonds, RankEnum.Ace, SuitEnum.Diamonds, -1)]
+        [InlineData(SuitEnum.Hearts, RankEnum.Jack, SuitEnum.Diamonds, RankEnum.King, SuitEnum.Diamonds, -1)]
+        [InlineData(SuitEnum.Hearts, RankEnum.Jack, SuitEnum.Diamonds, RankEnum.Queen, SuitEnum.Diamonds, -1)]
+        [InlineData(SuitEnum.Hearts, RankEnum.Jack, SuitEnum.Diamonds, RankEnum.Ten, SuitEnum.Diamonds, -1)]
+        [InlineData(SuitEnum.Hearts, RankEnum.Jack, SuitEnum.Diamonds, RankEnum.Nine, SuitEnum.Diamonds, -1)]
+        [InlineData(SuitEnum.Diamonds, RankEnum.Ace, SuitEnum.Diamonds, RankEnum.King, SuitEnum.Diamonds, -1)]
+        [InlineData(SuitEnum.Diamonds, RankEnum.Ace, SuitEnum.Diamonds, RankEnum.Queen, SuitEnum.Diamonds, -1)]
+        [InlineData(SuitEnum.Diamonds, RankEnum.Ace, SuitEnum.Diamonds, RankEnum.Ten, SuitEnum.Diamonds, -1)]
+        [InlineData(SuitEnum.Diamonds, RankEnum.Ace, SuitEnum.Diamonds, RankEnum.Nine, SuitEnum.Diamonds, -1)]
+        [InlineData(SuitEnum.Diamonds, RankEnum.King, SuitEnum.Diamonds, RankEnum.Queen, SuitEnum.Diamonds, -1)]
+        [InlineData(SuitEnum.Diamonds, RankEnum.King, SuitEnum.Diamonds, RankEnum.Ten, SuitEnum.Diamonds, -1)]
+        [InlineData(SuitEnum.Diamonds, RankEnum.King, SuitEnum.Diamonds, RankEnum.Nine, SuitEnum.Diamonds, -1)]
+        [InlineData(SuitEnum.Diamonds, RankEnum.Queen, SuitEnum.Diamonds, RankEnum.Ten, SuitEnum.Diamonds, -1)]
+        [InlineData(SuitEnum.Diamonds, RankEnum.Queen, SuitEnum.Diamonds, RankEnum.Nine, SuitEnum.Diamonds, -1)]
+        [InlineData(SuitEnum.Diamonds, RankEnum.Ten, SuitEnum.Diamonds, RankEnum.Nine, SuitEnum.Diamonds, -1)]
+        [InlineData(SuitEnum.Diamonds, RankEnum.Ace, SuitEnum.Clubs, RankEnum.Ace, SuitEnum.Diamonds, 1)]
+        [InlineData(SuitEnum.Diamonds, RankEnum.Ace, SuitEnum.Hearts, RankEnum.Ace, SuitEnum.Diamonds, 1)]
+        [InlineData(SuitEnum.Diamonds, RankEnum.Ace, SuitEnum.Spades, RankEnum.Ace, SuitEnum.Diamonds, 1)]
+        [InlineData(SuitEnum.Clubs, RankEnum.Ace, SuitEnum.Clubs, RankEnum.Ace, SuitEnum.Diamonds, 0)]
+        [InlineData(SuitEnum.Clubs, RankEnum.Ace, SuitEnum.Clubs, RankEnum.King, SuitEnum.Diamonds, -1)]
+        [InlineData(SuitEnum.Clubs, RankEnum.Ace, SuitEnum.Clubs, RankEnum.Queen, SuitEnum.Diamonds, -1)]
+        [InlineData(SuitEnum.Clubs, RankEnum.Ace, SuitEnum.Clubs, RankEnum.Jack, SuitEnum.Diamonds, -1)]
+        [InlineData(SuitEnum.Clubs, RankEnum.Ace, SuitEnum.Clubs, RankEnum.Ten, SuitEnum.Diamonds, -1)]
+        [InlineData(SuitEnum.Clubs, RankEnum.Ace, SuitEnum.Clubs, RankEnum.Nine, SuitEnum.Diamonds, -1)]
+        [InlineData(SuitEnum.Clubs, RankEnum.Ace, SuitEnum.Hearts, RankEnum.Ace, SuitEnum.Diamonds, 1)]
+        [InlineData(SuitEnum.Clubs, RankEnum.Ace, SuitEnum.Spades, RankEnum.Ace, SuitEnum.Diamonds, 1)]
+        [InlineData(SuitEnum.Hearts, RankEnum.Ace, SuitEnum.Spades, RankEnum.Ace, SuitEnum.Diamonds, 1)]
         public void Compare_DescendingSuitDescendingRank(SuitEnum card1Suit, RankEnum card1Rank, SuitEnum card2Suit,
-            RankEnum card2Rank, int expectedCompareValue)
+            RankEnum card2Rank, SuitEnum trumpSuit, int expectedCompareValue)
         {
             var card1 = new Card(
                 new Rank(card1Rank, null), 
-                new Suit(' ', card1Suit, SuitEnum.Unknown)
+                FromFactory(card1Suit)
                 );
             var card2 = new Card(
                 new Rank(card2Rank, null), 
-                new Suit(' ', card2Suit, SuitEnum.Unknown)
+                FromFactory(card2Suit)
                 );
 
-            var comparer = new NoTrumpHandSorter(false, false);
+            var comparer = new TrumpHandSorter(FromFactory(trumpSuit), false, false);
             var comparison = comparer.Compare(card1, card2);
             
             Assert.Equal(expectedCompareValue, comparison);
+        }
+
+        private static ISuit FromFactory(SuitEnum suit)
+        {
+            var factory = new SuitFactory();
+            return factory.GetSuit(suit);
         }
     }
 }
---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build Status](https://travis-ci.org/joe-meyer/dotnet-cards.svg?branch=develop)](https://travis-ci.org/joe-meyer/dotnet-cards)
